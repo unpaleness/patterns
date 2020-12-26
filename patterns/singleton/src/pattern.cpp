f0d848cbe30c22b6cfd4e38ba8d265bca7e1c03c
unpaleness <generalissimusunpaleness@gmail.com>
2020-12-27 01:49:19 +0300

    possible fix

--- patterns/singleton/src/pattern.cpp
+++ patterns/singleton/src/pattern.cpp
@@ -15,26 +15,26 @@ class Singleton {
  public:
   Singleton(const Singleton& other) = delete;
   void operator=(const Singleton& other) = delete;
-  static std::shared_ptr<Singleton> GetInstance(const size_t value);
+  static const Singleton* GetInstance(const size_t value);
   size_t GetValue() const { return value_; }
 
- protected:
+ private:
   Singleton(const size_t value) : value_(value) {
     std::cout << "Constructing Singleton with value " << value_ << '\n';
   }
 
  private:
-  static std::shared_ptr<Singleton> singleton_ptr_;
+  static const Singleton* singleton_ptr_;
   static std::mutex mutex_;
   size_t value_{0};
 };
 
-std::shared_ptr<Singleton> Singleton::singleton_ptr_;
+const Singleton* Singleton::singleton_ptr_;
 std::mutex Singleton::mutex_;
-std::shared_ptr<Singleton> Singleton::GetInstance(const size_t value) {
+const Singleton* Singleton::GetInstance(const size_t value) {
   std::lock_guard<std::mutex> lock(mutex_);
   if (!singleton_ptr_) {
-    singleton_ptr_ = std::make_shared<Singleton>(value);
+    singleton_ptr_ = new Singleton(value);
   }
   return singleton_ptr_;
 }
